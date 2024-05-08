a8911cd8ac1d9796f5c4c1138aed04c4c5b462ec
Ross Jennings <rossjjennings@gmail.com>
2024-05-07 23:34:33 -0400

    Extract configure() and get_server()

--- pia_service/connect.py
+++ pia_service/connect.py
@@ -23,7 +23,7 @@ def create_keypair():
     pubkey = result.stdout.strip()
     return key.decode('ascii'), pubkey.decode('ascii')
 
-def add_key(token, pubkey, cn, ip):
+def add_key(token, pubkey, server):
     """
     Request that a PIA WireGuard server add a public key.
 
@@ -31,9 +31,12 @@ def add_key(token, pubkey, cn, ip):
     ----------
     token: PIA authentication token
     pubkey: WireGuard public key
-    cn: Common name of WireGuard server
-    ip: IP address of WireGuard server
+    server: Dictionary representing WireGuard server
+     - key 'cn': Server common name
+     - key 'ip': Server IP address
     """
+    cn = server['cn']
+    ip = server['ip']
     session = requests.Session()
     session.mount(f'https://{cn}', DNSBypassAdapter(cn, ip))
     response = session.get(
@@ -43,32 +46,66 @@ def add_key(token, pubkey, cn, ip):
     )
     return response.json()
 
-def connect(args):
+def get_server(region, hostname=None):
     """
-    Connect to a PIA WireGuard server in the specified region.
+    Select and retrieve information about a WireGuard server from a specified
+    PIA region. If `hostname` is specified, choose the server with that name.
+    Otherwise, choose randomly from the available servers in the region.
+
+    Parameters
+    ----------
+    region: Name of a PIA region
+    hostname: (Optional) Hostname of preferred server
+
+    Returns
+    -------
+    region: Dictionary representing PIA region
+    server: Dictionary representing WireGuard server
+     - key 'cn': Server common name
+     - key 'ip': Server IP address
     """
     regions = get_regions()
-    region = regions[args.region]
-    if args.hostname is None:
-        wg_server = random.choice(region['servers']['wg'])
+    region = regions[region]
+    if hostname is None:
+        server = random.choice(region['servers']['wg'])
     else:
         wg_servers = {server['cn']: server for server in region['servers']['wg']}
-        wg_server = wg_servers[args.hostname]
+        server = wg_servers[hostname]
+    return region, server
 
+def configure(region, hostname=None, tailscale=False, disable_ipv6=True):
+    """
+    Set up a PIA WireGuard connection by creating a WireGuard keypair,
+    adding the public key to a specified PIA server, and filling in the
+    template WireGuard configuration.
+
+    Parameters
+    ----------
+    region: Name of a PIA region
+    hostname: (Optional) Hostname of preferred server
+    tailscale: Use Tailscale-specific configuration
+    disable_ipv6: Whether IPv6 is to be disabled (used only for status)
+
+    Returns
+    -------
+    config: WireGuard configuration file with server details filled in
+    status: Dictionary representing connection status
+    """
+    region, server = get_server(region, hostname)
     key, pubkey = create_keypair()
-
+    
     token = get_token()
     if token is None:
         # authentication failed, and we already printed the message
         return
 
-    result = add_key(token, pubkey, wg_server['cn'], wg_server['ip'])
+    result = add_key(token, pubkey, server)
     if not 'status' in result or not result['status'] == 'OK':
         print("Failed to add key to server. Response was:", file=sys.stderr)
         print(f"{result}", file=sys.stderr)
         print("Exiting.", file=sys.stderr)
         return
-    if args.allow_tailscale:
+    if tailscale:
         config_template = jinja_env.get_template('pia-tailscale.conf.jinja')
     else:
         config_template = jinja_env.get_template('pia.conf.jinja')
@@ -77,28 +114,49 @@ def connect(args):
         key=key,
         dns_servers=' '.join(ip for ip in result['dns_servers']),
         server_pubkey=result['server_key'],
-        endpoint=f"{wg_server['ip']}:{result['server_port']}",
+        endpoint=f"{server['ip']}:{result['server_port']}",
     )
 
     status = {
         'connection': {
             'pub_ip': result['server_ip'],
             'dns_servers': result['dns_servers'],
-            'disable_ipv6': not args.no_disable_ipv6,
-            'allow_tailscale': args.allow_tailscale,
+            'disable_ipv6': disable_ipv6,
+            'allow_tailscale': tailscale,
         },
         'wireguard': {
             'ip': result['peer_ip'],
             'server_ip': result['server_vip'],
+            'key': key,
+            'pubkey': pubkey,
+            'server_pubkey': result['server_key'],
         },
         'server': {
-            'region': args.region,
-            'hostname': wg_server['cn'],
-            'ip': wg_server['ip'],
+            'region': region,
+            'hostname': server['cn'],
+            'ip': server['ip'],
             'port': result['server_port'],
             'allows_port_forwarding': region['port_forward'],
         },
     }
+
+    return config, status
+
+def connect(args):
+    """
+    Connect to a PIA WireGuard server in the specified region.
+    """
+    # abort if already connected
+    if subprocess.run(["ip", "link", "show", "pia"], capture_output=True).returncode == 0:
+        print('Device "pia" already exists, aborting.', file=sys.stderr)
+        return
+
+    result = configure(args.region, args.hostname, tailscale=args.allow_tailscale,
+                       disable_ipv6=(not args.no_disable_ipv6))
+    config, status = result
+    if config is None:
+        # The above failed for some reason
+        return
 
     if not args.no_disable_ipv6:
         subprocess.run(["sudo", "sysctl", "-w", "net.ipv6.conf.all.disable_ipv6=1"])
